b190fa1dafe4cdab679d88810d52f3f0fb497ab3
ViacheslavKo <konyashovslavaa@mai.ru>
2020-02-16 17:08:50 +0300

    0.0.1 binarPoOblast

---
+++ TestBinarBredly/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace TestBinarBredly
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Open = new System.Windows.Forms.Button();
+            this.Binarization = new System.Windows.Forms.Button();
+            this.saveBinariz = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(919, 608);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(4, 47);
+            this.Open.Margin = new System.Windows.Forms.Padding(4);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(155, 42);
+            this.Open.TabIndex = 1;
+            this.Open.Text = "Open";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // Binarization
+            // 
+            this.Binarization.Enabled = false;
+            this.Binarization.Location = new System.Drawing.Point(4, 259);
+            this.Binarization.Margin = new System.Windows.Forms.Padding(4);
+            this.Binarization.Name = "Binarization";
+            this.Binarization.Size = new System.Drawing.Size(155, 42);
+            this.Binarization.TabIndex = 2;
+            this.Binarization.Text = "Binarization";
+            this.Binarization.UseVisualStyleBackColor = true;
+            this.Binarization.Click += new System.EventHandler(this.Binarization_Click);
+            // 
+            // saveBinariz
+            // 
+            this.saveBinariz.Enabled = false;
+            this.saveBinariz.Location = new System.Drawing.Point(3, 308);
+            this.saveBinariz.Name = "saveBinariz";
+            this.saveBinariz.Size = new System.Drawing.Size(155, 42);
+            this.saveBinariz.TabIndex = 4;
+            this.saveBinariz.Text = "Сохранить";
+            this.saveBinariz.UseVisualStyleBackColor = true;
+            this.saveBinariz.Click += new System.EventHandler(this.saveBinariz_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(3, 138);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(58, 23);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Укажите стороны d квадрата.";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.numericUpDown2.Location = new System.Drawing.Point(3, 3);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(60, 23);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            15,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(223, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "+ к значению яркости области d";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(69, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "%";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 99.13326F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 300F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 0.8667389F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.statusStrip1, 0, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90.42316F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.576838F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1236, 682);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel2.ColumnCount = 3;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 244F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.Open, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.saveBinariz, 0, 8);
+            this.tableLayoutPanel2.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.Binarization, 0, 7);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.numericUpDown1, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 6);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(930, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 11;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 72F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 243F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(294, 610);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 660);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(927, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 27.73109F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 72.26891F));
+            this.tableLayoutPanel3.Controls.Add(this.numericUpDown2, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.label3, 1, 0);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 208);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(238, 44);
+            this.tableLayoutPanel3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 687);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MinimumSize = new System.Drawing.Size(1250, 726);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Бинаризация Бредли";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.Button Binarization;
+        private System.Windows.Forms.Button saveBinariz;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
+
---
+++ TestBinarBredly/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace TestBinarBredly
+{
+    public partial class Form1 : Form
+    {
+        Bitmap imageOriginal, imageBinariz;
+        //string pathImg;
+        int width, height, d;
+        double[,] integralImage;
+
+        CancellationTokenSource cancelTokenStatus = new CancellationTokenSource();
+        CancellationToken tokenStatus;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Open_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open_dialog = new OpenFileDialog();
+            open_dialog.Filter = "Image Files(*.BMP;)(*.JPG;)|*.BMP;*.JPG;|All files (*.*)|*.*";
+            if (open_dialog.ShowDialog() == DialogResult.OK) //если в окне была нажата кнопка "ОК"
+            {
+                try
+                {
+                    imageOriginal = new Bitmap(open_dialog.FileName);
+
+                    width = imageOriginal.Width;
+                    height = imageOriginal.Height;
+                    integralImage = new double[imageOriginal.Width + 1, imageOriginal.Height + 1];
+
+                    pictureBox1.Image = imageOriginal;
+
+                    Binarization.Enabled = true;
+                    saveBinariz.Enabled = true;
+                }
+                catch
+                {
+                    DialogResult rezult = MessageBox.Show("Невозможно открыть выбранный файл",
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void saveBinariz_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save_dialog = new SaveFileDialog();
+            save_dialog.Filter = "Image Files(*.Bmp;)(*.Jpg;)|*.Bmp;*.Jpg;|All files (*.*)|*.*";
+            if (save_dialog.ShowDialog() == DialogResult.OK)
+            {
+                imageBinariz.Save(save_dialog.FileName);
+            }
+        }
+
+        private async void Binarization_Click(object sender, EventArgs e)
+        {
+            SetStatusAsync("Процесс бинарицации запущен. Ждите...", false);
+            imageBinariz = new Bitmap(width, height);
+            d = width / (int)numericUpDown1.Value;//4 - большое изображение, 8 - маленькое
+            saveBinariz.Enabled = false;
+            Open.Enabled = false;
+            Binarization.Enabled = false;
+            await Task.Run(() => IntegralImage());
+            pictureBox1.Image = imageBinariz;
+            Binarization.Enabled = true;
+            Open.Enabled = true;
+            saveBinariz.Enabled = true;
+            SetStatusAsync("Процесс бинарицации завершен.");
+        }
+
+        public void IntegralImage()
+        {
+            for (int i = 1; i <= width; i++)
+            {
+                for (int j = 1; j <= height; j++)
+                {
+                    double pixel = imageOriginal.GetPixel(i - 1, j - 1).GetBrightness();
+                    integralImage[i, j] = pixel + integralImage[i - 1, j] + integralImage[i, j - 1] - integralImage[i - 1, j - 1];
+
+                    if (i % d == 0 && j % d == 0)
+                    {
+                        BradlyBinarization(i, j);
+                    }
+                }
+            }
+
+            if (height % d != 0 && d != width)
+            {
+                int d2 = d;
+                for (int i = 0; i < width; i += d)
+                {
+                    BradlyBinarization(d2, height);
+                    d2 += d;
+                }
+            }
+
+        }
+
+        public double GetSrRectangleSum(int x, int y)
+        {
+            double sumLmObl = integralImage[x, y] + integralImage[x - d, y - d] - integralImage[x, y - d] - integralImage[x - d, y];
+            double srObl = sumLmObl / (d * d);
+            double procentOt_srObl = srObl * ((double)numericUpDown2.Value / 100);
+            return srObl + procentOt_srObl;
+        }
+
+        public void BradlyBinarization(int x, int y)
+        {
+            double porogVelich = GetSrRectangleSum(x, y);
+            for (int i = x - d + 1; i <= x; i++)
+            {
+                for (int j = y - d + 1; j <= y; j++)
+                {
+                    double pixel = imageOriginal.GetPixel(i - 1, j - 1).GetBrightness();
+                    if (pixel < porogVelich)
+                        imageBinariz.SetPixel(i - 1, j - 1, Color.Black);
+                    else
+                        imageBinariz.SetPixel(i - 1, j - 1, Color.White);
+                }
+            }
+        }
+
+        void SetStatusAsync(string Message, bool Hide = true)
+        {
+            cancelTokenStatus.Cancel();
+            Task.Run(() =>
+            {
+                Thread.Sleep(200);
+                cancelTokenStatus = new CancellationTokenSource();
+                tokenStatus = cancelTokenStatus.Token;
+
+                Action action = () => toolStripStatusLabel1.Text = Message;
+                try
+                {
+                    if (InvokeRequired)
+                        Invoke(action);
+                    else
+                        action();
+
+                    if (Hide)
+                    {
+                        DateTime DT = DateTime.Now.AddSeconds(5);
+
+                        while (DT.Subtract(DateTime.Now).TotalMilliseconds > 0 && !cancelTokenStatus.IsCancellationRequested)
+                            Thread.Sleep(100);
+
+                        if (!cancelTokenStatus.IsCancellationRequested)
+                        {
+                            action = () => toolStripStatusLabel1.Text = string.Empty;
+                            if (InvokeRequired)
+                                Invoke(action);
+                            else
+                                action();
+                        }
+                    }
+                }
+                catch { }
+            });
+        }
+
+    }
+}
+
